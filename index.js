ac0ecb8976995da10f6ae6f64b0661f726bc9c64
ahm-hasnat <ahm.hasnat.0.0@gmail.com>
2025-09-04 01:35:59 +0600

    project setup and get feature data

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xkximz0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+     const artifactCollection = client.db("artifactsDb").collection("artifacts");
+
+     app.get("/featured", async (req, res) => {
+      const artifacts = await artifactCollection.find().sort({ likes: 1 }).limit(6).toArray();
+      res.send(artifacts);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Digging artifact in server..!");
+});
+
+app.listen(port, () => {
+  console.log(`Artify server is running on port ${port}`);
+});
