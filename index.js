d30366ce3f40a7100c003085603c23565144db37
ahm-hasnat <ahm.hasnat.0.0@gmail.com>
2025-09-04 15:32:15 +0600

    details data loaded

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 
 
@@ -39,7 +39,12 @@ async function run() {
       res.send(allArtifacts);
     });
 
-
+       app.get("/allartifacts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await artifactCollection.findOne(query);
+      res.send(result);
+    });
 
 
     // Send a ping to confirm a successful connection
